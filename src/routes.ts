ccb99a22007ab298c0b2cd3a091851b90e6a195e
Max Base <MaxBaseCode@gmail.com>
2025-02-24 18:23:21 +0330

    change ident of location blocks

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,9 @@ import type { Route } from './type';
 
 export function generateRoutesConfig(routes: Route[]): string {
     return routes.map(route => `
-        location = ${route.path} {
-            if ($request_method = ${route.method}) {
-                proxy_pass http://${route.target};
-            }
-        }`).join('');
+    location = ${route.path} {
+        if ($request_method = ${route.method}) {
+            proxy_pass http://${route.target};
+        }
+    }`).join('');
 }
