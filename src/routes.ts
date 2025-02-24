55d6f010948d9c0f0ac4ffe80455fed2cae293bc
Max Base <MaxBaseCode@gmail.com>
2025-02-24 17:59:23 +0330

    nginx: [emerg] proxy_set_header directive is not allowed here in /etc/nginx/sites-enabled/9004:16

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,11 @@ import type { Route } from './type';
 
 export function generateRoutesConfig(routes: Route[]): string {
     return routes.map(route => `
-        location ${route.path} {
+        location = ${route.path} {
             if ($request_method = ${route.method}) {
-                proxy_set_header Host $host;
                 proxy_pass http://${route.target};
                 proxy_http_version 1.1;
+                proxy_set_header Host $host;
                 proxy_set_header Upgrade $http_upgrade;
                 proxy_set_header Connection "upgrade";
             }
