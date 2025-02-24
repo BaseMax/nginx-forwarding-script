bc0a1a7e19879956a182b7621512f2e33b66f72b
Max Base <MaxBaseCode@gmail.com>
2025-02-24 18:06:31 +0330

    update config

--- src/configRoutes.ts
+++ src/configRoutes.ts
@@ -10,6 +10,16 @@ export const config: { ports: PortConfig[] } = {
                 { method: 'PUT', path: '/user/update', target: '127.0.0.1:4561' },
                 { method: 'GET', path: '/transaction/get', target: '127.0.0.1:9761' }
             ]
+        },
+        {
+            port: 9005,
+            gateway: '127.0.0.1:3005',
+            routes: []
+        },
+        {
+            port: 9524,
+            gateway: '127.0.0.1:2524',
+            routes: []
         }
     ]
 };
