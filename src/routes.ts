c7ebbb65ae9f99f8a29aa0f1fb9adf16a6b795dc
Max Base <MaxBaseCode@gmail.com>
2025-02-25 23:03:14 +0330

    test

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import type { Route } from './type';
 
 export function generateRoutesConfig(routes: Route[]): string {
     return routes.map(route => `
-    location = ${route.path} {
+    location ${route.path} {
         if ($request_method = ${route.method}) {
             proxy_pass http://${route.target};
         }
