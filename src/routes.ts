37fff9fc300f98bff6d6f7c3bdc410be9923d5a9
Max Base <MaxBaseCode@gmail.com>
2025-02-25 23:02:36 +0330

    undo

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,14 @@
 import type { Route } from './type';
 
 export function generateRoutesConfig(routes: Route[]): string {
-    // return routes.map(route => `
-    // location = ${route.path} {
-    //     if ($request_method = ${route.method}) {
-    //         proxy_pass http://${route.target};
-    //     }
-    // }`).join('');
     return routes.map(route => `
-        location ${route.path} {
+    location = ${route.path} {
+        if ($request_method = ${route.method}) {
             proxy_pass http://${route.target};
-        }`).join('');
-    }
+        }
+    }`).join('');
+    // return routes.map(route => `
+    //     location ${route.path} {
+    //         proxy_pass http://${route.target};
+    //     }`).join('');
+}
