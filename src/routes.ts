a50bc08bdb7c9091da0602179c0e34ec9dd6c000
Max Base <MaxBaseCode@gmail.com>
2025-02-25 22:54:18 +0330

    test - delete method

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import type { Route } from './type';
 
 export function generateRoutesConfig(routes: Route[]): string {
+    // return routes.map(route => `
+    // location = ${route.path} {
+    //     if ($request_method = ${route.method}) {
+    //         proxy_pass http://${route.target};
+    //     }
+    // }`).join('');
     return routes.map(route => `
-    location = ${route.path} {
-        if ($request_method = ${route.method}) {
+        location ${route.path} {
             proxy_pass http://${route.target};
-        }
-    }`).join('');
-}
+        }`).join('');
+    }
