3c3f6f30ece16144692ebf73a0cdc4f6d73375c4
Max Base <MaxBaseCode@Gmail.Com>
2025-03-01 16:27:31 +0330

    Update configRoutes.ts

--- src/configRoutes.ts
+++ src/configRoutes.ts
@@ -6,8 +6,10 @@ export const config: { ports: PortConfig[] } = {
             port: 9004,
             gateway: '127.0.0.1:2004',
             routes: [
-                { method: 'POST', path: '/v1/member/register', target: '127.0.0.1:30031' },
-                { method: 'POST', path: '/v1/login', target: '127.0.0.1:30031' },
+                { method: '*', path: '/v1/member/register', target: '127.0.0.1:30031' },
+                { method: '*', path: '/v1/login', target: '127.0.0.1:30031' },
+                { method: '*', path: '/v1/verifycode', target: '127.0.0.1:30031' },
+                { method: '*', path: '/v1/getforgetcode', target: '127.0.0.1:30031' },
             ]
         },
         {
