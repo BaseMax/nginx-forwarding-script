93ab8fff77eb29d10c116c9792acbc9683fbc369
Max Base <MaxBaseCode@gmail.com>
2025-03-02 15:06:40 +0330

    undo nginx: [emerg] value 202 must be between 300 and 599 in /etc/nginx/sites-enabled/9004:24

--- src/generateNginxConfig.ts
+++ src/generateNginxConfig.ts
@@ -24,12 +24,6 @@ server {
         if ($request_method = OPTIONS) {
             return 204;
         }
-        proxy_intercept_errors on;
-        error_page 202 203 204 205 206 300 301 302 303 304 305 306 307 308 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 421 422 423 424 425 426 428 429 431 451 500 501 502 503 504 505 506 507 508 510 511 =200 @change_code;
-    }
-    
-    location @change_code {
-        return 200;
     }
 `;
 
